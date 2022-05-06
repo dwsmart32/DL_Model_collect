981d26b1aa500c0fbf8cc399dcff81a6837caa4a
이동욱 <dongwook@idong-ug-ui-MacBookPro.local>
2022-05-06 12:35:35 +0800

    bug fix v1.0.7

--- Plant_Seeding/test.py
+++ Plant_Seeding/test.py
@@ -17,8 +17,11 @@ def test_net(net, testloader, testdata_path, csv_path,device):
         label = label.to(device)
         print(path)
         _, y_pred = net(img).max(1)
-        row_numbers = df[df['file'] == img].index
-        df.loc[row_numbers, 'species'] = y_pred.cpu()
+
+        for i in range(0,len(path)):
+            filename = str(path[i].split(sep='\\')[-1])
+            row_numbers = df[df['file'] == filename].index
+            df.loc[row_numbers, 'species'] = y_pred.cpu()[i]
 
     #save csv file
     df.to_csv(csv_path, index=False)
